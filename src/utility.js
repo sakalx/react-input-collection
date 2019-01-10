29edba720a2321acbbfeb9daf35103dec2593179
sakal <sakals@mail.ua>
2019-01-10 16:51:13 -0500

    refactor: split code
    Signed-off-by: sakal <sakals@mail.ua>

--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,6 @@
+const setCssProp = (prop, value) => document.documentElement.style.setProperty(prop, value);
+const getCssVarName = (cssId, prop) => `--${cssId}-${prop}`;
+
 export function initCssVariables(cssIdVariable) {
 
   const cssVariables = new Map([
@@ -14,9 +17,9 @@ export function initCssVariables(cssIdVariable) {
         const currValue = cssVariables.get(prop);
 
         if (value !== currValue) {
-          const cssVarKey = `--${cssIdVariable}-${prop}`;
+          const key = getCssVarName(cssIdVariable, prop);
 
-          document.documentElement.style.setProperty(cssVarKey, value);
+          setCssProp(key, value);
           cssVariables.set(prop, value)
         }
       }
@@ -30,8 +33,8 @@ export function replaceColors(cssIdVariable) {
   return function (objProps) {
     for (const prop in objProps) {
       if (!!prop && objProps.hasOwnProperty(prop)) {
-        const cssVarKey = `--${cssIdVariable}-${prop}`;
-        document.documentElement.style.setProperty(cssVarKey, objProps[prop]);
+        const key = getCssVarName(cssIdVariable, prop);
+        setCssProp(key, objProps[prop]);
       }
     }
   }
